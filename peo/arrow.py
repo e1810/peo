77434482ec1faac803e9c54350b6d1a2a2548baa
e1810 <v.iceele1810@gmail.com>
2021-10-07 21:22:46 +0900

    矢印が取る幅を削減

--- peo/arrow.py
+++ peo/arrow.py
@@ -1,19 +1,39 @@
 import re
 
-class DepthManager:
-	depths = []
+
+class ArrowManager:
 	def __init__(self, n: int):
-		self.depths = [0] * n
-	def max(self, s: int, e: int) -> int:
-		ret = 0
-		for i in range(s, e+1):
-			ret = max(ret, self.depths[i])
+		self.arrows = [[' '] for i in range(n)]
+		self.depth = 1
+
+	def min_empty_col(self, l: int, r: int) -> int:
+		emp = [True for i in range(self.depth)]
+		emp[0] = False
+		for i in range(l, r+1):
+			for j in range(min(self.depth, len(self.arrows[i]))):
+				emp[j] = emp[j] and (self.arrows[i][j] not in ['\u2502', '\u2514', '\u250c'])
+		for i in range(self.depth):
+			if emp[i]: return i
+		return self.depth
+	
+	def add_arrow(self, s: int, e: int, col: int):
+		self.depth = max(self.depth, col+1)
+		outarrow = list('<' + '\u2500' * (col-1) + ['\u2514','\u250c'][s<e])
+		inarrow = list('>' + '\u2500' * (col-1) + ['\u2514','\u250c'][s>e])
+		self.arrows[s] = outarrow + self. arrows[s][1+col:]
+		self.arrows[e] = inarrow + self.arrows[e][1+col:]
+		if s > e: s, e = e, s
+		for i in range(s+1, e):
+			while len(self.arrows[i]) <= col:
+				self.arrows[i].append(' ')
+			self.arrows[i][col] = '\u2502'
+	
+	def output(self):
+		ret = []
+		for row in self.arrows:
+			adjusted_row = row + [' '] * (self.depth - len(row))
+			ret.append(list(reversed(adjusted_row)))
 		return ret
-	
-	def update(self, s: int, e: int, x: int) -> int:
-		for i in range(s, e+1):
-			self.depths[i] = x
-	
 
 
 def add_flow_arrow(msgs: str) -> str:
@@ -37,22 +57,16 @@ def __arrowing_in_func(insts: str) -> list:
 	# 基本的に逆から見ていく  矢印終点に辿り着いたら始点まで戻る形で矢を張る
 	# 終点が同じ2つの矢があると上のやつしかできない 要修正
 
-	arrows = [[' '] for i in range(len(insts))]
-	depthM = DepthManager(len(insts))
+	arrowM = ArrowManager(len(insts))
 	e2b = dict()
+	# 下から上への矢印を処理
 	for i in range(len(insts)-1, -1, -1):
 		addr = insts[i][0][:-1]
 		if addr in e2b:
 			st = e2b[addr]
-			depth = depthM.max(i, st)
-			arrows[i] = list('>' + '\u2500' * depth + '\u250c') + arrows[st][2+depth:]
-			for j in range(i+1, st):
-				while len(arrows[j]) <= depth + 1:
-					arrows[j].append(' ')
-				arrows[j][depth+1] = '\u2502'
-			arrows[st] = list('<' + '\u2500' * depth + '\u2514') + arrows[i][2+depth:]
-			depthM.update(i, st, depth+2)
-
+			depth = arrowM.min_empty_col(i, st)
+			arrowM.add_arrow(st, i, depth)
+			
 		if len(insts[i]) < 3 or len(insts[i][2].split()) == 1: continue
 		opc, opr, *_ = insts[i][2].split()
 		if opc[0] != 'j': continue
@@ -65,15 +79,9 @@ def __arrowing_in_func(insts: str) -> list:
 		addr = insts[i][0][:-1]
 		if addr in e2b:
 			st = e2b[addr]
-			depth = depthM.max(st, i)
-			arrows[st] = list('<' + '\u2500' * depth + '\u250c') + arrows[st][2+depth:]
-			for j in range(st+1, i):
-				while len(arrows[j]) <= depth + 1:
-					arrows[j].append(' ')
-				arrows[j][depth+1] = '\u2502'
-			arrows[i] = list('>'+ '\u2500' * depth + '\u2514') + arrows[i][2+depth:]
-			depthM.update(st, i, depth+2)
-
+			depth = arrowM.min_empty_col(st, i)
+			arrowM.add_arrow(st, i, depth)
+		
 		if len(insts[i]) < 3 or len(insts[i][2].split()) == 1: continue
 		opc, opr, *_ = insts[i][2].split()
 		if opc[0] != 'j': continue
@@ -81,12 +89,7 @@ def __arrowing_in_func(insts: str) -> list:
 		if int(opr, 16) < int(addr, 16): continue
 		e2b[opr] = i
 
-	depth = depthM.max(0, len(insts)-1)
-	print(depth)
-	for i in range(len(insts)):
-		arrows[i] += [' '] * (depth - len(arrows[i]))
-		arrows[i].reverse()
-		print(arrows[i])
-		insts[i] = [''.join(arrows[i])] + insts[i]
+	for i, arrowrow in enumerate(arrowM.output()):
+		insts[i] = [''.join(arrowrow)] + insts[i]
 	return insts
 	
