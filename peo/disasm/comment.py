af36d388972bb523e0bcfce4a8e5f08bcd3aaccc
d4wnin9 <d4wnin9@gmail.com>
2021-10-17 09:17:30 +0900

    Update comment.py
    文字が一つかどうかなんて関係なく変換できればしてできなければbyteを文字として出力

--- peo/disasm/comment.py
+++ peo/disasm/comment.py
@@ -21,16 +21,7 @@ class Comment:
                 if "movabs" in msg[2]:
                     long_str_little = int(msg[2].split(",")[1], 16)
                     plain_str = long_str_little.to_bytes((long_str_little.bit_length() + 7) // 8, byteorder='little').decode('utf-8', 'backslashreplace')
-                    
-                    if long_str_little >= 0x20:
-                        
-                        if long_str_little <= 0x7e:
-                            plain_str = chr(long_str_little)
-
-                        else:
-                            plain_str = long_str_little.to_bytes((long_str_little.bit_length() + 7) // 8, byteorder='little').decode('utf-8', 'backslashreplace')
-
-                        self.msgs[i].append(f"; '{plain_str}'")
+                    self.msgs[i].append(f"; '{plain_str}'")
             except IndexError:
                 pass
 
@@ -39,15 +30,8 @@ class Comment:
             try:
                 if "mov" in msg[2] and "ORD" in msg[2].split(" ")[1]:
                     long_str_little = int(msg[2].split(",")[1], 16)
-                    if long_str_little >= 0x20:
-                        if long_str_little <= 0x7e:
-                            plain_str = chr(long_str_little)
-
-                        else:
-                            plain_str = long_str_little.to_bytes((long_str_little.bit_length() + 7) // 8, byteorder='little').decode('utf-8', 'backslashreplace')
-
-                        self.msgs[i].append(f"; '{plain_str}'")
-                    
+                    plain_str = long_str_little.to_bytes((long_str_little.bit_length() + 7) // 8, byteorder='little').decode('utf-8', 'backslashreplace')
+                    self.msgs[i].append(f"; '{plain_str}'")
             except IndexError:
                 pass
             except ValueError:
